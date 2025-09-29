0325ad2284c46eaf6693f7a626e5320785b6d6f1
cainiaoup <18847931709@163.com>
2025-09-29 09:35:11 +0800

    remove finetune from repo

--- finetune/src/open_r1/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .import_utils import is_e2b_available
-from .model_utils import get_tokenizer
-from .script_utils import rank0_print
-
-__all__ = ["get_tokenizer", "is_e2b_available", "rank0_print"]
